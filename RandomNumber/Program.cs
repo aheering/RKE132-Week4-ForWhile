4fbbb57e8103f2c544b884630b2f1e7d50c09edb
aheering <heering.agnes@gmail.com>
2023-04-28 16:34:52 +0300

    add week4 code

---
+++ RandomNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿// Juhuslikud numbrid ja FOR-tsükkel
+
+Random rnd = new Random();
+int myRandomNum;
+int randomSum = 0;
+
+for(int i = 0; i < 3; i++)
+{
+    myRandomNum = rnd.Next(0, 11); // 0 - 10, otspunkt pole kaasa arvatud
+    randomSum = randomSum + myRandomNum;
+    Console.WriteLine($"My random number is: {myRandomNum}");
+}
+Console.WriteLine($"Sum of the random numbers: {randomSum}");
